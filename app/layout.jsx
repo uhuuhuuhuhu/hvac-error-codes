5e19dbcaf62ea5c023e588bee6ac57c53fa23e51
uhuuhuuhuhu <since2021sm@gmail.com>
2026-09-06 00:32:23 +0900

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,7 +2,7 @@ export const metadata = {
   title: 'HVAC 에러코드 조회 | 에러 코드 진단 및 해결 가이드',
   description: 'HVAC 에러 코드를 빠르게 검색하고 원인 및 해결 방법을 확인하세요.',
   verification: {
-    google: '<meta name="google-site-verification" content="o-543sv7N8YvctB1Q8v6BAPLuv2vSdQ4J5E1Zs06A1I" />',
+    google: content="o-543sv7N8YvctB1Q8v6BAPLuv2vSdQ4J5E1Zs06A1I",
   },
 };
 
